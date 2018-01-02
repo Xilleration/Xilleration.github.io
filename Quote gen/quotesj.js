5b466aa04c8eb894467730d7afe26f8e7adf1ade
Xilleration <b.ledford92@gmail.com>
2018-01-02 17:11:59 -0600

    messing with quote

--- Quote gen/quotesj.js
+++ Quote gen/quotesj.js
@@ -1,6 +1,9 @@
 // JavaScript Document
 var quotes = [
-	'Your favorite MTG format using only Standard Cards! - Motumbo'
-	'Can we customize our 404? - David'
-	''
+	'Your favorite MTG format using only Standard Cards! - Motumbo',
+	'Can we customize our 404? - David',
+	'...Yeah, I like that... - David',
+	'Are we banning the bouncy gods? - Brandon',
+	'Are you guys winning? - Motumbo',
+	'Do I have to use a backslash on each of these?!... - Brandon'
 ]
