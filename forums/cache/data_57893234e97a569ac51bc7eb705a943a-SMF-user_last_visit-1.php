acbe532f43db10be5a8e998a8c25f8de4f9b6431
DrMedford <35042638+DrMedford@users.noreply.github.com>
2018-01-03 11:49:51 -0600

    Website Build 1/3/18

---
+++ forums/cache/data_57893234e97a569ac51bc7eb705a943a-SMF-user_last_visit-1.php
@@ -0,0 +1,1 @@
+<?php if (!defined('SMF')) die; if (1515016335 < time()) $expired = true; else{$expired = false; $value = 's:1:"1";';}?>
---
+++ forums/cache/data_57893234e97a569ac51bc7eb705a943a-SMF-user_last_visit-2.php
@@ -0,0 +1,1 @@
+<?php if (!defined('SMF')) die; if (1515015844 < time()) $expired = true; else{$expired = false; $value = 's:1:"1";';}?>
